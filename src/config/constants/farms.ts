44135616abb9ec8e765ff8e5748af55de829e854
BourbonDefi <80276717+bourbondefi@users.noreply.github.com>
2022-08-15 12:08:13 -0400

    add dogeburn

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -24,40 +24,55 @@ const farms: FarmConfig[] = [
       97: '',
       2000: '0xcA1424b1d64FD6364E2717c9539106da50300c3A', // WADA-USDC Bourbon-LP
     },
-    tokenSymbol: 'DOGE',
+    tokenSymbol: 'YUZU',
     tokenAddresses: {
       97: '',
-      2000: '0xB7ddC6414bf4F5515b52D8BdD69973Ae205ff101',
+      2000: '0xa98fa09D0BED62A9e0Fb2E58635b7C9274160dc7',
     },
     quoteTokenSymbol: QuoteToken.DOGE,
     quoteTokenAdresses: contracts.wada,
   },
   {
     pid: 2,
-    lpSymbol: 'YUZU-DOGE',
+    lpSymbol: 'DOGESHREK-DOGE',
     lpAddresses: {
       97: '',
       2000: '0x0629C3E894D63084c8feaAB8c738Faf38aA0563b', // WADA-USDC Bourbon-LP
     },
-    tokenSymbol: 'DOGE',
+    tokenSymbol: 'DOGESHREK',
     tokenAddresses: {
       97: '',
-      2000: '0xB7ddC6414bf4F5515b52D8BdD69973Ae205ff101',
+      2000: '0x2BE0096B24343549E34224aa9aa297E99961023D',
     },
     quoteTokenSymbol: QuoteToken.DOGE,
     quoteTokenAdresses: contracts.wada,
   },
   {
     pid: 3,
-    lpSymbol: 'YUZU-DOGE',
+    lpSymbol: 'OMNOM-DOGE',
     lpAddresses: {
       97: '',
       2000: '0x3fC0A08974D7f6a22a0f8D63eD60B4D935b53F5A', // WADA-USDC Bourbon-LP
     },
-    tokenSymbol: 'DOGE',
+    tokenSymbol: 'OMNOM',
     tokenAddresses: {
       97: '',
-      2000: '0xB7ddC6414bf4F5515b52D8BdD69973Ae205ff101',
+      2000: '0xe3fcA919883950c5cD468156392a6477Ff5d18de',
+    },
+    quoteTokenSymbol: QuoteToken.DOGE,
+    quoteTokenAdresses: contracts.wada,
+  },
+  {
+    pid: 4,
+    lpSymbol: 'DOGEBURN-DOGE',
+    lpAddresses: {
+      97: '',
+      2000: '0x65464190C1585A707A14360eeeD25B8211a56225', // WADA-USDC Bourbon-LP
+    },
+    tokenSymbol: 'DOGEBURN',
+    tokenAddresses: {
+      97: '',
+      2000: '0xeE1DaaED7E25cD40AeaF7E45a51e1F4b2e28A993',
     },
     quoteTokenSymbol: QuoteToken.DOGE,
     quoteTokenAdresses: contracts.wada,
